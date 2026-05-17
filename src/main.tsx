8a0edfbf6453f714d38959a1abd96e3c424011ea
WJZ_P <1369727119@qq.com>
2026-05-17 14:37:32 +0000

    fix: 修复渲染器 ASI 陷阱导致 root.render is not a function 崩溃

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-// Use contextBridge
-(window as any).ipcRenderer.on('main-process-message', (_event: any, message: any) => {
+;(window as any).ipcRenderer.on('main-process-message', (_event: any, message: any) => {
   console.log(message)
 })
